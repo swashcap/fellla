671273e1da955e144a0ff9ad9cef282d092ba017
Cory Reed <swashcap@gmail.com>
2019-12-08 15:03:04 -0800

    refactor: refine `TextInput` styles

--- src/components/controls/TextInput.tsx
+++ src/components/controls/TextInput.tsx
@@ -1,11 +1,12 @@
-import { FunctionComponent, JSX } from 'preact';
+import { JSX } from 'preact';
 import { createComponent } from 'preact-fela';
-import { AppTheme, theme } from '../../theme';
+import { AppTheme } from '../../theme';
 
 export type TextInputProps = JSX.HTMLAttributes<HTMLInputElement>;
 
 export const TextInput = createComponent<TextInputProps>(
   ({ disabled, theme }: TextInputProps & { theme: AppTheme }) => ({
+    background: theme.colors.background,
     borderColor: disabled ? theme.colors.disabled : theme.colors.text,
     borderRadius: '100em',
     borderStyle: 'solid',
@@ -13,11 +14,19 @@ export const TextInput = createComponent<TextInputProps>(
     color: disabled ? theme.colors.disabled : theme.colors.text,
     display: 'block',
     font: 'inherit',
+    marginBottom: 0,
+    marginTop: 0,
     padding: `${theme.spacing.small} ${theme.spacing.normal}`,
     width: '100%',
 
     '::placeholder': {
       color: theme.colors.disabled,
+    },
+
+    ':focus': {
+      boxShadow: `0 0 0 2px ${theme.colors.accent}`,
+      borderColor: theme.colors.accent,
+      outline: 'none',
     },
   }),
   'input',
