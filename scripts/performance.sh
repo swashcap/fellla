414b0dee62282574ece493c4c7efeebe33400d7c
Cory Reed <swashcap@gmail.com>
2019-12-08 14:48:11 -0800

    refactor: skip perf check for non-source changing commits

--- scripts/performance.sh
+++ scripts/performance.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+# Exit early if no source files changed
+if [ "$(git diff --cached --name-only | grep -Ecm 1 '^src' || true)" = 0 ]; then
+  exit
+fi
+
 BUDGET=70000
 SCRIPTS=$(find dist -type f -name '*.js')
 
