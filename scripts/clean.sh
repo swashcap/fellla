1cb966ad941e6934c525992b43d01bfb6ca5278d
Cory Reed <swashcap@gmail.com>
2019-12-08 18:44:47 -0800

    fix: remove compiled output from rsg on clean

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 rm -rf .parcel-cache dist &
-find src -type f \( -name '*.js' -o -name '*.js.map' -o -name '*.d.ts' \) \
+find {rsg,src} -type f \( -name '*.js' -o -name '*.js.map' -o -name '*.d.ts' \) \
   -exec rm {} \+ &
 
 wait
