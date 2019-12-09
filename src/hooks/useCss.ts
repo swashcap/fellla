5329943dda6867c01820a0f3fd6e23f6e909518b
Cory Reed <swashcap@gmail.com>
2019-12-08 18:43:18 -0800

    feat: pass props to `renderRule`

--- src/hooks/useCss.ts
+++ src/hooks/useCss.ts
@@ -9,11 +9,11 @@ import { useTheme } from './useTheme';
  * A port of `useFela`'s CSS helper for preact.
  * https://github.com/robinweser/fela/blob/master/packages/react-fela/src/useFela.js
  */
-export const useCss = () => {
+export const useCss = (props?: any) => {
   const renderer = useContext<IRenderer>(RendererContext);
   const theme = useTheme();
 
   return (...styles: Array<any>) => {
-    return renderer.renderRule(combineRules(...styles), { theme });
+    return renderer.renderRule(combineRules(...styles), { theme, ...props });
   };
 };
